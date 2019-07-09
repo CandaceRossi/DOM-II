7c906a2715a8afda060927bd212f2b6107f60672
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-07-09 17:45:43 -0400

    added background color change with key down

--- js/indexNew.js
+++ js/indexNew.js
@@ -1,32 +1,36 @@
-//added mouse over mouse out on nav a tags
+//1.added mouseover on nav a tags
 let nav = document.querySelector('nav');
 nav.addEventListener("mouseover", function(event) {
     event.target.style.color = "red";
 });
 
+//2.added mouseout on nav a tags
+
 nav.addEventListener("mouseout", function(event) {
     event.target.style.color = "black";
 });
+
+//3.added mouse wheel animation on logo
 let funBus = document.querySelector('body');
 funBus.addEventListener('wheel', function(event) {
     TweenMax.to('.logo-heading', 1, { x: 100 });
 });
-
+//4.added mouse wheel animation on heading
 let heading1 = document.querySelector('body');
 heading1.addEventListener('wheel', function(event) {
     TweenMax.to('.intro h2', 3, { x: 220 });
 });
-
+//5.added mouse wheel animation on smaller headings
 let headings = document.querySelector('body');
 headings.addEventListener('wheel', function(event) {
     TweenMax.to('.text-content h2, .content-destination h2', 3, { x: 100 });
 });
+//6.added background color change with mouse wheel
 let color = document.querySelector('body');
 color.addEventListener('wheel', function(event) {
     color.style.backgroundColor = 'lightblue';
-
 });
-
+//7.added zoom in and out feature to main img
 function zoom(event) {
 
     event.preventDefault();
@@ -43,3 +47,12 @@ function zoom(event) {
 let scale = 1;
 const img = document.querySelector('img');
 img.onwheel = zoom;
+//8.added background color change with keydown
+color.addEventListener('keydown', function(event) {
+    color.style.backgroundColor = 'green';
+});
+//9.added y axis shift in 'p' with wheel
+let words = document.querySelector('body');
+words.addEventListener('wheel', function(event) {
+    TweenMax.to('p', 2, { y: 40 });
+});
