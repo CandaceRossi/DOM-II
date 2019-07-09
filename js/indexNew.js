4303d99b5ee742cc0c9370aa1c187b4c408d6fcd
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-07-09 17:34:57 -0400

    added background color change and image zoom

--- js/indexNew.js
+++ js/indexNew.js
@@ -7,7 +7,39 @@ nav.addEventListener("mouseover", function(event) {
 nav.addEventListener("mouseout", function(event) {
     event.target.style.color = "black";
 });
-let funBus = document.querySelector('.logo-heading');
-funBus.addEventListener('click', function(event) {
+let funBus = document.querySelector('body');
+funBus.addEventListener('wheel', function(event) {
     TweenMax.to('.logo-heading', 1, { x: 100 });
 });
+
+let heading1 = document.querySelector('body');
+heading1.addEventListener('wheel', function(event) {
+    TweenMax.to('.intro h2', 3, { x: 220 });
+});
+
+let headings = document.querySelector('body');
+headings.addEventListener('wheel', function(event) {
+    TweenMax.to('.text-content h2, .content-destination h2', 3, { x: 100 });
+});
+let color = document.querySelector('body');
+color.addEventListener('wheel', function(event) {
+    color.style.backgroundColor = 'lightblue';
+
+});
+
+function zoom(event) {
+
+    event.preventDefault();
+
+    scale += event.deltaY * -0.01;
+
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    // Apply scale transform
+    img.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const img = document.querySelector('img');
+img.onwheel = zoom;
